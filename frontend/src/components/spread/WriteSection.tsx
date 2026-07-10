646cc7b96e03574a42ce9a1894e21581b5be9e9c
mochizuki-seito <s2522041@stu.musashino-u.ac.jp>
2026-07-10 14:45:47 +0900

    一応

--- frontend/src/components/spread/WriteSection.tsx
+++ frontend/src/components/spread/WriteSection.tsx
@@ -213,7 +213,6 @@ export function WriteSectionRight({ form }: { form: WriteForm }) {
                 form.setResolveDate(addMonthsISO(todayISO(), 1));
               }
             }}
-            
           />{" "}
           日付は未定(6ヶ月後にたずねます)
         </label>
